4b33b7b747c364d23150ffd24f9e146178acb2b2
hellenrt <101749518+hellenrt@users.noreply.github.com>
2022-07-11 12:04:04 +0200

    PCA Funktionen und Anfang Visualisierung PCA

--- Functions/PCA.py
+++ Functions/PCA.py
@@ -1,11 +1,15 @@
 import numpy as np
+import seaborn as sb
+import pandas as pd
 
 def z_transformation(set , single_image):
     """
     centering and scaling, returns transformed dataset and single image for comparison
+
     :param set: training dataset, no labels
     :param single_image: image for classification from test dataset
     """
+    
     std0 = []
     for i in range(0, set.shape[1]):
         if np.std(set[:, i]) == 0:
@@ -24,9 +28,11 @@ def z_transformation(set , single_image):
 def PCA(clean_set, clean_img, num_components=10):
     """
     principal component analysis, returns array of dataset and single image with defined number of components
+
     :param clean_set: training dataset (2D-Array; no label; after z-transformation)
     :param clean_img: single image (1D-Array; no label; after z-transformation)
     :param num_components): number of components (integer)
+    :return: returns dataset and single image with reduced dimensions and PCs as features
     """
 
     #variance
@@ -52,20 +58,50 @@ def PCA(clean_set, clean_img, num_components=10):
 
 def create_sorted_eigenvec(clean_set, num_components = 10):
     """
-    
-    
+    needed for PCA of only the dataset or image
+
+    :param clean_set: training dataset (2D-Array; no label; after z-transformation)
+    :param num_components: number of components (integer); default is set to 10
+    :return: 2D-Array of sorted Eigenvectors
     """
+
     cov_arr = np.cov(clean_set, rowvar = False)
+    eigen_val, eigen_vec = np.linalg.eigh(cov_arr)
     index_sorted = np.argsort(eigen_val)[::-1]
     sorted_eigenvec = eigen_vec[:,index_sorted]
     return sorted_eigenvec
 
 
 def image_only(clean_img, sorted_eigenvec):
-    img_reduced = np.dot(eigenvec_subset.transpose(), clean_img.transpose()).transpose()
+    """
+    create principal components for one image
+    
+    :param clean_img: image from either daset (1D-Array; no label; after z-transformation)
+    :param sorted_eigenvec: 2D-Array of sorted eigenvectors from train or test dataset
+    :return: image with reduced dimensions and PCs as features
+    """
+
+    img_reduced = np.dot(sorted_eigenvec.transpose(), clean_img.transpose()).transpose()
     return img_reduced
 
 
 def set_only(clean_set, sorted_eigenvec):
-    set_reduced = np.dot(eigenvec_subset.transpose(), clean_set.transpose()).transpose()
+    """
+    create principal components for dataset
+    
+    :param clean_set: training dataset (2D-Array; no label; after z-transformation)
+    :param sorted_eigenvec: 2D-Array of sorted eigenvectors from train or test dataset
+    :return: dataset with reduced dimensions and PCs as features
+    """
+
+    set_reduced = np.dot((sorted_eigenvec).transpose(), clean_set.transpose()).transpose()
     return set_reduced
+
+
+def visualize_2d(reduced_dataset, i=0, j=1):
+    """
+    scatterplot of principal images based on principal components
+
+    :param reduced_dataset: dataset with PCs as features
+    """
+    pca_df = 
